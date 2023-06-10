c7b117e34a7c58d48088769fff8e1e01fdae6198
Maria Hlusneac <hlusmari@amazon.com>
2023-06-10 23:21:05 +0300

    Added a flask server that serves requests for generating images based on captions.

---
+++ code/templates/server.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/save_captions', methods=['POST'])
+def save_captions():
+    caption = request.form['caption']
+
+    with open('../../data/coco/example_captions.txt', 'a') as file:
+        file.write(caption + '\n')
+
+    image_path = "../../models/coco_AttnGAN2/example_captions"
+    os.rmdir(image_path)
+    os.system("python ../main.py --cfg cfg/eval_coco.yml --gpu -1")
+
+    return send_file(os.path.join(image_path, '/0_s_0_g2.png'), mimetype='image/png')
+
+if __name__ == '_main_':
+    app.run(port=80)
